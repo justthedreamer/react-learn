91dce5b962c9ce4722c972aa9e810259f11d6fd4
Piotr Zapala <piotr.zapala.dev@gmail.com>
2023-08-24 23:09:26 +0200

    Styles - Operators

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className='App'>
-      <Job salary = {9000} position = "Senior SDE" company = "Amazon"/>
-      <Job salary = {12000} position = "Junior SDE" company = "Google"/>
-      <Job salary = {10000} position = "Project Menager" company = "Netflix"/>
-    </div>
+  const age = 17;
+  const isGreen = true;
 
-  );
-}
-
-const Job = (props) =>{
   return (
-    <div>
-      <h1>{props.position}</h1>
-      <h2>{props.company} {props.salary}$</h2>
+    <div className="App">
+      {age >= 18 ? <h1>OverAge</h1> : <h1>Under Age</h1>}
+      
+      <h1 style = {{color: isGreen? "green" : "red", backgroundColor: 'black' }}>This has color</h1>
+
+      {isGreen && <button>Click me</button>}
     </div>
   );
 }
