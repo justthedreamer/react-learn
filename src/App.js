5fcd9ea8861ca842f8e1167d804ba3ba2fcdf8c5
Piotr Zapala <piotr.zapala.dev@gmail.com>
2023-08-25 00:51:41 +0200

    CRUD #1 Todo List

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import './App.css';
 import {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  return <div className='App'>
-    <button onClick={()=>{setCount(count +1)}}>Increase</button>
-    <button onClick={()=>{setCount(count -1)}}>Decrease</button>
-    <button onClick={()=>{setCount(0)}}>Set to zero</button>
+const [todoList,setTodoList] = useState([])
+const [newTask,setTask]= useState("");
 
-    <h1>{count}</h1>
+const handleChange = (event) =>{
+  setTask(event.target.value);
+}
+
+const addTask = () =>{
+  setTodoList([...todoList, newTask]);
+}
+
+return (<div className='App'>
+    <div className='addTask'>
+        <input type="text" onChange={handleChange} />
+        <button onClick={addTask}>Add task</button>
+    </div>
+    <div className='list'>
+        {todoList.map((task)=> {
+          return <p className='list-item'>{task}</p>
+        })}
+    </div>
   </div>
+);
 }
 
 export default App;
