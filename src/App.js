527b5a9efe06cc81956fe37fbed93d460b971df7
Piotr Zapala <piotr.zapala.dev@gmail.com>
2023-08-29 23:45:28 +0200

    Fetching data from APIs #1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import {useState} from "react";
-import {Text} from "./Text"
+import Axios  from 'axios';
 
 function App() {
-  const [showText,setShowText] = useState(false);
+
+  const [catFact,setCatFact] = useState("")
+
+  const fetchCatFact = () =>{
+    Axios.get('https://catfact.ninja/fact').then((res)=>{
+      setCatFact(res.data.fact);
+    })
+  }
+
+  useEffect(()=>{
+    Axios.get('https://catfact.ninja/fact').then((res)=>{
+      setCatFact(res.data.fact);
+    })
+  },[])
 
   return (
     <div className='App'>
-      <button onClick={()=>{
-        setShowText(!showText);
-      }}>Show Text</button>
-      {showText && <Text/>}
+      <button onClick={fetchCatFact}>Generate Cat Fact</button>
+      <p>{catFact}</p>
     </div>
   )
 }
